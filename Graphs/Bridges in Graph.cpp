e093073a2c25fe794511c548b8196d4440577877
RakeshRoxx <rakeshmandal651@gmail.com>
2021-12-29 15:19:02 +0530

    bridges in graph

---
+++ Graphs/Bridges in Graph.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> inTime, lowTime;
+vector<bool> vis;
+int timer = 1;
+
+void dfs(int src, int parent, vector<int> adj[]) {
+	vis[src] = true;
+	inTime[src] = lowTime[src] = timer++;
+	for (int child : adj[src]) {
+		if (child == parent) continue;
+		if (vis[child]) {
+			lowTime[src] = min(lowTime[src], inTime[child]);
+		} else {
+			dfs(child, src, adj);
+			if (lowTime[child] > inTime[src]) {
+				cout << src << " -> " << child << " is a bridge.\n";
+			}
+			lowTime[src] = min(lowTime[src], lowTime[child]);
+		}
+	}
+}
+
+void solution() {
+	int N, E;
+	cin >> N >> E;
+	vector<int> adj[N + 1];
+	while (E--) {
+		int u, v;
+		cin >> u >> v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	inTime.resize(N + 1);
+	lowTime.resize(N + 1);
+	vis.resize(N + 1, false);
+	dfs(1, 0, adj);
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	solution();
+	return 0;
+}
