9f1958ca2d5f6c7d0a17d8b6b2c25f4f4210b454
RakeshRoxx <rakeshmandal651@gmail.com>
2022-01-02 11:48:23 +0530

    Recursion

---
+++ Recursion/q2_print 1 to N.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n) {
+    if (n == 0) return;
+    print(n - 1);
+    cout << n << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    print(n);
+    return 0;
+}
